d341abf092ca0ecbc45c118eaa9298813b854c7b
EDIIIZ <ediz.tefenlilioglu@gmail.com>
2023-01-18 12:42:40 +0100

    Add Types

---
+++ src/lib/types/types.ts
@@ -0,0 +1,104 @@
+type product = {
+  webcode: string;
+  price: [
+    {
+      price: number;
+      branchName: string;
+      branchId: number;
+    }
+  ];
+};
+
+type googleSearchApiResponse = {
+  kind: string;
+  url: {
+    type: string;
+    template: string;
+  };
+  queries: {
+    request: [
+      {
+        count: number;
+        startIndex: number;
+        inputEncoding: string;
+        outputEncoding: string;
+        safe: string;
+        cx: string;
+        exactTerms: string;
+      }
+    ];
+  };
+  searchInformation: {
+    searchTime: number;
+    formattedSearchTime: string;
+    totalResults: string;
+    formattedTotalResults: string;
+  };
+  items: [
+    {
+      kind: string;
+      title: string;
+      htmlTitle: string;
+      link: string;
+      displayLink: string;
+      snippet: string;
+      htmlSnippet: string;
+      cacheId: string;
+      formattedUrl: string;
+      htmlFormattedUrl: string;
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: string;
+            width: number;
+            height: number;
+          }
+        ];
+        metatags: [
+          {
+            pagetype: string;
+            viewport: string;
+            storefinderresultfilter: string;
+            'csrf-token': string;
+            ak_screentype: string;
+            'format-detection': string;
+          }
+        ];
+        cse_image: [
+          {
+            src: string;
+          }
+        ];
+        listitem: [
+          {
+            item: string;
+            name: string;
+            position: number;
+          },
+          {
+            item: string;
+            name: string;
+            position: number;
+          },
+          {
+            item: string;
+            name: string;
+            position: number;
+          },
+          {
+            item: string;
+            name: string;
+            position: number;
+          },
+          {
+            item: string;
+            name: string;
+            position: number;
+          }
+        ];
+      };
+    }
+  ];
+};
+
+export type { product, googleSearchApiResponse };
