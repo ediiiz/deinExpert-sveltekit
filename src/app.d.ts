b98f2f030172c3862c2e3feaaaa15803bebf7d79
EDIIIZ <ediz.tefenlilioglu@gmail.com>
2023-01-21 19:26:01 +0100

    update declare

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,16 +2,3 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {}
-
-declare global {
-  namespace App {
-    interface Platform {
-      env?: {
-        preisdetektivKV: KVNamespace;
-        preisdetektivR2: DurableObjectNamespace;
-      };
-    }
-  }
-}
-
-export {};
