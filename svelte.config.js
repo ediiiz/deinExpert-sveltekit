0cb22e8518a23142a504c863cbeac2744da1522c
EDIIIZ <ediz.tefenlilioglu@gmail.com>
2023-01-21 19:20:06 +0100

    adapter auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
