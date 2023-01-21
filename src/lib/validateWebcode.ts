21dd9e6e58bdc89791638ea207af37b3307f1861
EDIIIZ <ediz.tefenlilioglu@gmail.com>
2023-01-21 19:09:52 +0100

    update

--- src/lib/validateWebcode.ts
+++ src/lib/validateWebcode.ts
@@ -1,10 +1,8 @@
 import { env } from '$env/dynamic/private';
 import type { googleSearchApiResponse } from '$lib/types/types';
 
-const API_KEY = platform.env.preisdetektivKV.get('SECRET_API_KEY');
-const SEARCH_ENGINE_ID = platform.env.preisdetektivKV.get(
-  'SECRET_SEARCH_ENGINE_ID'
-);
+const API_KEY = env.SECRET_API_KEY;
+const SEARCH_ENGINE_ID = env.SECRET_SEARCH_ENGINE_ID;
 
 const googleSearchUrl = `https://www.googleapis.com/customsearch/v1/siterestrict?key=${API_KEY}&cx=${SEARCH_ENGINE_ID}&exactTerms=`;
 
