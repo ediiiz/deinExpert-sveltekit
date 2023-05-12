1afdc9e5de0930ad14556c2bba4576762927bc73
EDIIIZ <ediz.tefenlilioglu@gmail.com>
2023-05-12 12:26:45 +0200

    refactor / add mobilecashback case

--- src/routes/product/[webcode]/affiliate.js
+++ src/routes/product/[webcode]/affiliate.js
@@ -23,60 +23,73 @@ async function fetchCashback() {
       method: 'post',
     };
 
-    // @ts-ignore
-    let response = await fetch(`${corsProxy}${topcashbackShare}`, requestOptions);
-    // @ts-ignore
-    let headers = JSON.parse(response.headers.get('cors-received-headers'))
-    let data = await response.text();
-    let tcShareCookies = parseAllCookies(headers['set-cookie'].split(/,\W(?=\D)/g));
-    const xCorsHeaders1 = await generateCookieObject({ CookieObjects: tcShareCookies, headers: xCorsHeaders });
-    let document = new DOMParser().parseFromString(data, 'text/html');
-    // @ts-ignore
-    const earnCashback = document.querySelector('html body div.gecko-main.gecko-text-center div.gecko-single-container div.gecko-m15em div.cont-to-merch-wrapper a.gecko-btn-cont.gecko-btn-cont-primary').href.toString().replace(`${window.location.protocol}//${window.location.host}`, 'https://www.topcashback.de');
 
-
-    ///  Request to earncashback.aspx to get redirect.aspx link
-    myHeaders['x-cors-headers'] = JSON.stringify(xCorsHeaders1);
-    // @ts-ignore
-    response = await fetch(`${corsProxy}${earnCashback}`, requestOptions);
-    // @ts-ignore
-    headers = JSON.parse(response.headers.get('cors-received-headers'))
-    data = await response.text();
-    let redirect = `https://www.topcashback.de${response.headers.get('location')}`
-    let earnCashbackCookies = parseAllCookies(headers['set-cookie'].split(/,\W(?=\D)/g));
-    const xCorsHeaders2 = await generateCookieObject({ CookieObjects: earnCashbackCookies, headers: xCorsHeaders });
-
-
-    if (redirect) {
-      ///  Request to redirect to get awin1 link
-      myHeaders['x-cors-headers'] = JSON.stringify(xCorsHeaders2);
+    async function getEarnCashbackLink() {
       // @ts-ignore
-      response = await fetch(`${corsProxy}${redirect}`, requestOptions);
+      const response = await fetch(`${corsProxy}${topcashbackShare}`, requestOptions);
       // @ts-ignore
-      headers = JSON.parse(response.headers.get('cors-received-headers'))
-      data = await response.text();
-      document = new DOMParser().parseFromString(data, 'text/html');
+      const headers = JSON.parse(response.headers.get('cors-received-headers'))
+      const data = await response.text();
+      const tcShareCookies = parseAllCookies(headers['set-cookie'].split(/,\W(?=\D)/g));
+      const xCorsHeaders1 = await generateCookieObject({ CookieObjects: tcShareCookies, headers: xCorsHeaders });
+      const document = new DOMParser().parseFromString(data, 'text/html');
       // @ts-ignore
-      const awin = document.querySelector('html body form#form1 div#pnlContainer.container div div#pnlMainContent div#show-redirect.continue div a#hypRedirectMerchant').href.toString();
-
-      
-
-      // awin = https://www.awin1.com/cread.php?clickref=9343892x96139359&awinmid=30007&awinaffid=637873&p=http://google.de <- add parameter p and append the product url
-
-      // /// Request to awin to get expert affiliate link
-      // myHeaders['x-cors-headers'] = JSON.stringify(xCorsHeaders);
-
-      // // @ts-ignore
-      // response = await fetch(`${corsProxy}${awin}`, requestOptions);
-      // // @ts-ignore
-      // headers = JSON.parse(response.headers.get('cors-received-headers'));
-      // // @ts-ignore
-      // redirect = response.headers.get('location').split('?')[1];
-      // // @ts-ignore
-      // let awin1Cookies = parseAllCookies(headers['set-cookie'].split(/,\W(?=\D)/g));
-
-      return awin;
+      const earnCashback = document.querySelector('html body div.gecko-main.gecko-text-center div.gecko-single-container div.gecko-m15em div.cont-to-merch-wrapper a.gecko-btn-cont.gecko-btn-cont-primary').href.toString().replace(`${window.location.protocol}//${window.location.host}`, 'https://www.topcashback.de');
+      return { earnCashback, xCorsHeaders1 };
     }
+
+    // @ts-ignore
+    async function getRedirectLink(xCorsHeaders1, earnCashback) {
+      myHeaders['x-cors-headers'] = JSON.stringify(xCorsHeaders1);
+      // @ts-ignore
+      const response = await fetch(`${corsProxy}${earnCashback}`, requestOptions);
+      // @ts-ignore
+      const headers = JSON.parse(response.headers.get('cors-received-headers'))
+      // @ts-ignore
+      const data = await response.text();
+      const redirect = `https://www.topcashback.de${response.headers.get('location')}`
+      const mobileRedirect = redirect;
+      if (redirect.includes('mobile')) {
+        // @ts-ignore
+        const { redirect, xCorsHeaders2 } = await getRedirectLink(xCorsHeaders1, mobileRedirect);
+        return { redirect, xCorsHeaders2 };
+      }
+      const earnCashbackCookies = parseAllCookies(headers['set-cookie'].split(/,\W(?=\D)/g));
+      const xCorsHeaders2 = await generateCookieObject({ CookieObjects: earnCashbackCookies, headers: xCorsHeaders });
+      return { redirect, xCorsHeaders2 };
+    }
+
+    // @ts-ignore
+    async function getAwinLink(redirect, xCorsHeaders2) {
+      const mobileRedirect = redirect;
+      // if redirect contains the string "mobile" then redirect to the mobile site
+      if (redirect.includes('mobile')) {
+        // @ts-ignore
+        const { redirect, xCorsHeaders2 } = await getRedirectLink(xCorsHeaders1, mobileRedirect);
+
+      } else {
+        ///  Request to redirect to get awin1 link
+        myHeaders['x-cors-headers'] = JSON.stringify(xCorsHeaders2);
+        // @ts-ignore
+        const response = await fetch(`${corsProxy}${redirect}`, requestOptions);
+        // @ts-ignore
+        // @ts-ignore
+        const headers = JSON.parse(response.headers.get('cors-received-headers'))
+        const data = await response.text();
+        const document = new DOMParser().parseFromString(data, 'text/html');
+        // @ts-ignore
+        const awin = document.querySelector('html body form#form1 div#pnlContainer.container div div#pnlMainContent div#show-redirect.continue div a#hypRedirectMerchant').href.toString();
+
+        return awin;
+      }
+    }
+
+    const { earnCashback, xCorsHeaders1 } = await getEarnCashbackLink();
+    const { redirect, xCorsHeaders2 } = await getRedirectLink(xCorsHeaders1, earnCashback);
+    const awin = await getAwinLink(redirect, xCorsHeaders2);
+
+    return awin;
+
 
   } catch (error) {
     console.log(error);
