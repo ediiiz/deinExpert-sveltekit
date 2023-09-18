234b4a3a1d8311e7bbcecebd355f0e638581705d
EDIIIZ <ediz.tefenlilioglu@gmail.com>
2023-09-18 16:12:45 +0200

    restructure types

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -1,14 +1,11 @@
 type product = {
   webcode: string;
   url: string;
-  price: [
-    {
-      price: number;
-      branchName: string;
-      branchId: number;
-    }
-  ];
-  verify: string;
+  price: {
+    price: number;
+    branchName: string;
+    branchId: number;
+  }[];
 };
 
 type googleSiteVerifyResponse = {
@@ -44,71 +41,70 @@ type googleSearchApiResponse = {
     totalResults: string;
     formattedTotalResults: string;
   };
-  items: [
-    {
-      kind: string;
-      title: string;
-      htmlTitle: string;
-      link: string;
-      displayLink: string;
-      snippet: string;
-      htmlSnippet: string;
-      cacheId: string;
-      formattedUrl: string;
-      htmlFormattedUrl: string;
-      pagemap: {
-        cse_thumbnail: [
-          {
-            src: string;
-            width: number;
-            height: number;
-          }
-        ];
-        metatags: [
-          {
-            pagetype: string;
-            viewport: string;
-            storefinderresultfilter: string;
-            'csrf-token': string;
-            ak_screentype: string;
-            'format-detection': string;
-          }
-        ];
-        cse_image: [
-          {
-            src: string;
-          }
-        ];
-        listitem: [
-          {
-            item: string;
-            name: string;
-            position: number;
-          },
-          {
-            item: string;
-            name: string;
-            position: number;
-          },
-          {
-            item: string;
-            name: string;
-            position: number;
-          },
-          {
-            item: string;
-            name: string;
-            position: number;
-          },
-          {
-            item: string;
-            name: string;
-            position: number;
-          }
-        ];
-      };
-    }
-  ];
+  items:
+  {
+    kind: string;
+    title: string;
+    htmlTitle: string;
+    link: string;
+    displayLink: string;
+    snippet: string;
+    htmlSnippet: string;
+    cacheId: string;
+    formattedUrl: string;
+    htmlFormattedUrl: string;
+    pagemap: {
+      cse_thumbnail: [
+        {
+          src: string;
+          width: number;
+          height: number;
+        }
+      ];
+      metatags: [
+        {
+          pagetype: string;
+          viewport: string;
+          storefinderresultfilter: string;
+          'csrf-token': string;
+          ak_screentype: string;
+          'format-detection': string;
+        }
+      ];
+      cse_image: [
+        {
+          src: string;
+        }
+      ];
+      listitem: [
+        {
+          item: string;
+          name: string;
+          position: number;
+        },
+        {
+          item: string;
+          name: string;
+          position: number;
+        },
+        {
+          item: string;
+          name: string;
+          position: number;
+        },
+        {
+          item: string;
+          name: string;
+          position: number;
+        },
+        {
+          item: string;
+          name: string;
+          position: number;
+        }
+      ];
+    };
+  }[]
 };
 
 export type { product, googleSearchApiResponse, googleSiteVerifyResponse };
