7ea9a751d3082ff389e80852a70a1fee5e17dd02
EDIIIZ <ediz.tefenlilioglu@gmail.com>
2023-09-18 17:24:19 +0200

    restyle product

--- src/lib/components/Product.svelte
+++ src/lib/components/Product.svelte
@@ -9,14 +9,11 @@
   import { Button } from '$lib/components/ui/button';
 </script>
 
-<article class="bg-gray-300 rounded-lg flex flex-col p-2">
-  <header class="flex items-center justify-center text-center py-4">
-    {products.productName}
+<article class="bg-gray-100 rounded-md flex flex-col p-2 shadow-2xl pb-4">
+  <header class="flex items-center justify-center align-middle text-center">
+    <div class="flex py-4 h-20 items-center justify-center text-lg">{products.productName}</div>
   </header>
-  <Button href="/product/{products.webcode}" class=""
+  <Button href="/product/{products.webcode}" class="text-lg text-center"
     >Ab {products?.priceHistory[0]?.price.sort((a, b) => a.price - b.price)[0]?.price || '...'}€</Button
   >
-  <div class="text-sm text-center py-2">
-    <Time relative timestamp={products.priceHistory[0].date} />
-  </div>
 </article>
